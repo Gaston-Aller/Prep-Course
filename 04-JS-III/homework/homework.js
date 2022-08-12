15ae870c383cbf5cd4fbe7423c987146daf4c92d
gaston <gaston7001@gmail.com>
2022-08-12 18:42:53 -0300

    finish homework JS 3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -182,7 +182,7 @@ function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
-  // Tu código:a
+  // Tu código:
   var nuevoArray = [];
   for(let i= 0; i<array.length; i++) {
     if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
