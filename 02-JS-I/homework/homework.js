b8a00e784d7f10c07283230a6029ac9273878e02
gaston <gaston7001@gmail.com>
2022-08-13 15:03:49 -0300

    tarea JS 1 termianda

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -6,7 +6,7 @@ const nuevaString = 'Hola';
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 2;
 
-// Crea una variable booleana:
+// Crea una variable booleanaa:
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
